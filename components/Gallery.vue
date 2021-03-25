78eccf37a6460e6508cdb64e80dc961f5030ef6a
afrinsarker <afrinsarker91@gmail.com>
2021-03-25 22:13:15 +0600

    style changes

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="block galleryBlock">
     <v-container>
-      <h2 class="text-center">Gallery</h2>
       <v-row>
-        <v-col v-for="item in items" :key="item.id" class="d-flex child-flex" cols="6" sm="4">
+        <v-col v-for="item in items" :key="item.id" class="d-flex child-flex" cols="12" sm="4">
           <v-hover v-slot="{ hover }" open-delay="200" >
           <v-card flat tile class="d-flex mx-auto" :elevation="hover ? 16 : 100" :class="{ 'on-hover': hover }">
             <v-img :src="item.src" aspect-ratio="1" class="grey lighten-2">
